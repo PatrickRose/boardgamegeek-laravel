c71dd9a8d6d9599cfd2c4ea7e93e02fdc5856243
Allan <allan@codetoplay.com>
2014-05-24 17:29:11 +1000

    Adding thumbnail support

--- src/PatrickRose/BoardGameGeek/BoardGame.php
+++ src/PatrickRose/BoardGameGeek/BoardGame.php
@@ -35,7 +35,8 @@ class BoardGame {
     }
     $attributes['description'] = $boardGame->description->__toString();
     $attributes['image'] = $boardGame->image->__toString();
-
+    $attributes['thumbnail'] = $boardGame->thumbnail->__toString();
+    
     return new BoardGame($attributes);
   }
 
